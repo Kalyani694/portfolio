d7f338e1f15e56c939892faa36c11d7daa8e895f
Kalyani694 <kalyaniborase54@gmail.com>
2025-01-22 15:52:18 +0530

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,8 +9,9 @@ const Navbar = () => {
         <div className="wrapper">
             <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale: 1}} transition={{duration:0.5}} >Kalyani Borase</motion.span>
             <div className="social">
-                <a href="https://www.facebook.com/share/p/19kHetVMc8/"><img src="/facebook.png" alt="" /></a>
-                <a href="https://www.instagram.com/p/DD95rRTuvkq/?igsh=MWprNmcyM2VvaDhkYg=="><img src="/instagram.png" alt="" /></a>
+            <a href="https://leetcode.com/u/kalyaniborase/"><img src="https://shopallpremium.com/wp-content/uploads/2022/02/LeetCode_logo_rvs.png" alt="" /></a>
+            <a href="https://www.hackerrank.com/profile/borasek694"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs2XugPqx2uT1Jy3Utrp8ZwUdBEZ0PB6B39A&s" alt="" /></a>
+            <a href="https://www.naukri.com/code360/profile/3640816d-f281-4eac-ac96-f97920a86c2d"><img src="https://yt3.googleusercontent.com/l3rB5iGz7zH2JQzZKZ3gBI-8oow6edAquxvQapNtNhy__VdbIDHxbvJJGT8YuvVkqjoQXDeS5w=s900-c-k-c0x00ffffff-no-rj" alt="" /></a>
                 <a href="https://github.com/Kalyani694/"><img src="https://static-00.iconduck.com/assets.00/github-icon-2048x2048-iw6ut8fe.png" alt="" /></a>
                 <a href="https://www.linkedin.com/in/kalyani-borase-708213244/"><img src="/linkedin.webp" alt="" /></a>
             </div>
