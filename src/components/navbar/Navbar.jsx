2cf567f1e2033bbc66fcb35c9d561a888fea10ec
Kalyani694 <kalyaniborase54@gmail.com>
2025-05-22 10:51:39 +0530

    update the file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className="social">
             <a href="https://leetcode.com/u/kalyaniborase/"><img src="https://shopallpremium.com/wp-content/uploads/2022/02/LeetCode_logo_rvs.png" alt="" /></a>
             <a href="https://www.hackerrank.com/profile/borasek694"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs2XugPqx2uT1Jy3Utrp8ZwUdBEZ0PB6B39A&s" alt="" /></a>
-            <a href="https://www.naukri.com/code360/profile/3640816d-f281-4eac-ac96-f97920a86c2d"><img src="https://yt3.googleusercontent.com/l3rB5iGz7zH2JQzZKZ3gBI-8oow6edAquxvQapNtNhy__VdbIDHxbvJJGT8YuvVkqjoQXDeS5w=s900-c-k-c0x00ffffff-no-rj" alt="" /></a>
+            <a href="https://www.naukri.com/code360/profile/3640816d-f281-4eac-ac96-f97920a86c2d"><img src="https://play-lh.googleusercontent.com/76gEFhQto5xMHr2Qf8nWLvm1s0O60clhkwHvxQDSeI3hthf7Zs05JJQeyg5H347DGQ" alt="" /></a>
                 <a href="https://github.com/Kalyani694/"><img src="https://static-00.iconduck.com/assets.00/github-icon-2048x2048-iw6ut8fe.png" alt="" /></a>
                 <a href="https://www.linkedin.com/in/kalyani-borase-708213244/"><img src="/linkedin.webp" alt="" /></a>
             </div>
